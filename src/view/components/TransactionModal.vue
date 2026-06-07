530b02b0d219c42a2e24faf1e5f40e1e305e1680
cyberfolk <andrea1995ferrari@gmail.com>
2026-06-07 16:13:56 +0200

    [FEAT] VIEW: ListView + MatrixView

---
+++ src/view/components/TransactionModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="modal-overlay" @click.self="emit('close')">
+    <div class="modal">
+      <p>transazioni {{ fromId }} → {{ toId }} (stub)</p>
+      <button @click="emit('close')">Chiudi</button>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({ fromId: { type: String, required: true }, toId: { type: String, required: true } });
+const emit = defineEmits(['close']);
+</script>
